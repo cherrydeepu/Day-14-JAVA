5653b59dd725a96f941f573d1956a09adcc36870
Rishitha <trishithapandu2218@gmail.com>
2024-12-15 12:27:05 +0530

    Add files via upload

---
+++ LambdaThread.java
@@ -0,0 +1,8 @@
+//Creating thread using lambda expression
+public class LambdaThread{
+    public static void main(String args[]){
+        Runnable r=()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+    }
+}
