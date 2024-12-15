2a560e837da96116178f382f9a1d414eccd95027
Rishitha <trishithapandu2218@gmail.com>
2024-12-15 12:30:05 +0530

    Add files via upload

---
+++ AliveMethod.java
@@ -0,0 +1,8 @@
+public class AliveMethod{
+    public static void main(String args[]){
+        Runnable r=()->System.out.println("I am new from java8 version");
+        Thread t=new Thread(r);
+        t.start();
+        System.out.println(t.isAlive());
+    }
+}
